b4142645ddd13dd585d51b0c3846046bd7a24616
NoaEXC530 <145761457+NoaEXC530@users.noreply.github.com>
2024-01-17 15:14:38 +0000

    Neue

---
+++ Lernen/ProbearbeitRuben.java
@@ -0,0 +1,15 @@
+public class ProbearbeitRuben {
+
+
+    public static void main(String[] args) {
+        //Eingabe
+            double a = 9.81; 
+            double t = 1.5;
+            double s = 0;
+        //Verabreitung
+            s=1/2*a*(t*t);
+        //Ausgabe
+            System.out.println("Die gebrauchte Zeit bertägt" + t + "Sekunden"."Dies entspricht einer Höhe von" + s + "Metern.");
+
+    }
+}
